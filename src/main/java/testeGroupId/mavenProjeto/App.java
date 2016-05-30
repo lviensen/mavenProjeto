39fe1f96b256999598bb371c9a87d1e9c12db6a4
Aluno <Aluno@politecnico.local>
2016-05-30 13:58:14 -0300

    Comeco maven

---
+++ src/main/java/testeGroupId/mavenProjeto/App.java
@@ -0,0 +1,13 @@
+package testeGroupId.mavenProjeto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
